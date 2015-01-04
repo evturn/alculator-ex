cded584dc7c315394f9aa584dc93cdbb5c709afe
Evan Turner <evturn@gmail.com>
2015-01-03 19:11:57 -0500

    app.params added

--- app.js
+++ app.js
@@ -11,29 +11,54 @@ app = express();
 logger = require('./logger');
   app.use(logger);
 
-app.get('/blocks', function(request, response) {
-  var blocks = ['Fixed', 'Movable', 'Rotating'];
-  if (request.query.limit >= 0) {
-    response.json(blocks.slice(0, request.query.limit));
-  } else {
-  response.json(blocks);  
-  }
-});
+only_get = require('./only_get');
+  app.use(only_get);
 
 var blocks = {
   'Fixed': 'Gonna stay where it is',
   'Movable': 'Could be moved',
   'Rotating': 'Moving in a circle'
 };
+
+var locations = {
+  'Fixed': 'First floor', 'Movable': 'Second floor', 'Rotating': 'Penthouse'
+};
+
+//Used for dynamic routing
+app.param('name', function(request, response, next) {
+  var name = request.params.name;
+  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  request.blockName = block;
+  next();
+});
+
+
+app.get('/blocks', function(request, response) {
+  response.json(Object.keys(blocks));  
+});
+
+
 app.get('/blocks/:name', function(request, response) {
-  var description = blocks[request.params.name];
+  var description = blocks[request.blockName];
   response.json(description);
-  if (!description) {
+  if(!description) {
     response.status(404).json('We aint got ' + request.params.name);
   } else {
     response.json(description);
   }
 });
+
+
+app.get('/locations/:name', function(request, response) {
+  var location = locations[request.blockName];
+  response.json(location);
+  if(!location) {
+    response.status(404).json('We aint got ' + request.params.name);
+  } else {
+    response.json(location);
+  }
+});
+
 
 
 
